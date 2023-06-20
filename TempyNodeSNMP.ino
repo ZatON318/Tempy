971d946afc105490a1d158887cc02ba06e92cbb4
ZatON318 <81118558+ZatON318@users.noreply.github.com>
2023-06-20 10:49:49 +0200

    Update TempyNodeSNMP.ino

--- TempyNodeSNMP.ino
+++ TempyNodeSNMP.ino
@@ -2,15 +2,21 @@
 #include <ETH.h>
 #include <WiFiUdp.h>
 #include <DallasTemperature.h>
-#include <PubSubClient.h>
 #include <SNMP_Agent.h>
 
-SNMPAgent snmp = SNMPAgent("public");
+SNMPAgent snmp = SNMPAgent("private");
 WiFiUDP udp;
 SNMPTrap* testTrap = new SNMPTrap("public", SNMP_VERSION_2C);
 
 TimestampCallback* timestampCallback;
 int tensOfMillisCounter = 0;
+
+  int testint = 10;
+  int test = 10;
+
+  char tempchar[10];  
+  char* outputchar = "0.00";
+
 
 
 #define ONE_WIRE_BUS 4 // definuje pin na ktorom je pripojeny teplotny senzor
@@ -72,27 +78,20 @@ void setupSNMP()
 {
   snmp.setUDP(&udp);
   snmp.begin();
-
-  int testint = 5;
   
-  //Adiciona o OID para umidade (apenas leitura)
-  snmp.addIntegerHandler(".1.3.6.1.4.1.12345.0", &testint, false);
-  //Adiciona o OID para temperatura (apenas leitura)
-  //snmp.addStringHandler(".1.3.6.1.4.1.12345.1", &strTemperature, false);
-  //Adiciona o OID para umidade máxima (leitura e escrita)
-  //snmp.addIntegerHandler(".1.3.6.1.4.1.12345.2", &maxHumidity, true);
-  //Adiciona o OID para umidade mínima (leitura e escrita)
-  //snmp.addIntegerHandler(".1.3.6.1.4.1.12345.3", &minHumidity, true);
+  
+  snmp.addIntegerHandler(".1.3.6.1.4.1.12345.0", &testint);
+  snmp.addIntegerHandler(".1.3.6.1.2.1.1.1.0", &test, false);
+  snmp.addReadWriteStringHandler(".1.3.6.1.4.1.12345.1", &outputchar, false);
 }
 
 void setup()
 {
   Serial.begin(115200);
-  Serial.println("BOOTING TempyNode_v1.1");
-  Serial.println("I turned myself into pickle Morty , I AM PICKLE RICK !!!");
   WiFi.onEvent(WiFiEvent);
   ETH.begin();
   ETH.setHostname("TempyNode_v1.1");
+  Serial.println(ETH.localIP());
   setupSNMP();
 
   sensors.begin(); 
@@ -103,31 +102,16 @@ void loop()
 {
   snmp.loop(); 
 
-  // Update our timestamp value
   tensOfMillisCounter = millis()/10;
   
-  // Send the trap to the specified IP address
-  
-  IPAddress destinationIP = IPAddress(10, 65, 1, 86);
-  
-  if(snmp.sendTrapTo(testTrap, destinationIP, true, 2, 5000) != INVALID_SNMP_REQUEST_ID){ 
-      Serial.println("Sent SNMP Trap");
-  } else {
-      Serial.println("Couldn't send SNMP Trap");
-      Serial.println(ETH.localIP());
-  }
-  
-  //char tempString[8];
-  //dtostrf(getTemp(), 1, 2, tempString);
-  
-  //delay(5000);
+  getTemp();
+  delay(10);
 }
 
-float getTemp(){
-   Serial.print(" \nTemp req..."); 
-   sensors.requestTemperatures();
-   Serial.print("Teplota je: "); 
-   Serial.print(sensors.getTempCByIndex(0));
-  
-   return(sensors.getTempCByIndex(0));
+float getTemp(){ 
+  sensors.requestTemperatures();
+  float temp = sensors.getTempCByIndex(0);
+  dtostrf(temp, 5, 2, tempchar);
+  outputchar = tempchar;
+  return 0;
   }
